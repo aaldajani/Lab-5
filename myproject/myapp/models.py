e8114d4973f6c0fc9936a15d4346766cc8026d90
aaldajani <aldajani.ab@gmail.com>
2024-05-15 23:46:39 +0300

    a

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return self.username
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Person
 
-# Create your views here.
+people = []
+
+def add(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        new_person = Person(username=username, password=password)
+        people.append(new_person)
+        return HttpResponseRedirect('/')
+    return render(request, 'add.html')
+
+def index(request):
+    return render(request, 'index.html', {'people': people})
+
